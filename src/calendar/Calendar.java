44d31456cd93e983d0ceaea5f830cee08620b784
MJbae <bmj5208@gmail.com>
2020-12-04 11:59:34 +0900

    mission1: println 사용해서 출력하기

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -2,6 +2,17 @@ package calendar;
 
 public class Calendar {
 	public static void main(String[] args) {
-		System.out.println("test");
+		char[] dayOfWeek = { '일', '월', '화', '수', '목', '금', '토' };
+
+		for (char eachDay : dayOfWeek) {
+			System.out.print(eachDay + "  ");
+		}
+		System.out.println("\n---------------------");
+		for (int date = 1; date < 29; date++) {
+			System.out.printf("%3d", date);
+			if (date % 7 == 0) {
+				System.out.println(" ");
+			}
+		}
 	}
 }
