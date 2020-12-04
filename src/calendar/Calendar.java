1ce42488296aac0d6177bb1fe3a14836e227284a
MJbae <bmj5208@gmail.com>
2020-12-04 13:35:38 +0900

    mission3: 특정 달의 최대 일수 출력하기

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -1,18 +1,30 @@
 package calendar;
 
+import java.util.Scanner;
+
 public class Calendar {
 	public static void main(String[] args) {
-		char[] dayOfWeek = { '일', '월', '화', '수', '목', '금', '토' };
+//		char[] dayOfWeek = { '일', '월', '화', '수', '목', '금', '토' };
+//
+//		for (char eachDay : dayOfWeek) {
+//			System.out.print(eachDay + "  ");
+//		}
+//		System.out.println("\n---------------------");
+//		for (int date = 1; date < 29; date++) {
+//			System.out.printf("%3d", date);
+//			if (date % 7 == 0) {
+//				System.out.println(" ");
+//			}
+//		}
 
-		for (char eachDay : dayOfWeek) {
-			System.out.print(eachDay + "  ");
-		}
-		System.out.println("\n---------------------");
-		for (int date = 1; date < 29; date++) {
-			System.out.printf("%3d", date);
-			if (date % 7 == 0) {
-				System.out.println(" ");
-			}
-		}
+		int[] maxDayOfMonth = { 31, 28, 30, 31, 30, 31, 31, 31, 30, 31, 30, 31 };
+
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("달을 입력하세요.");
+		int inputMonth = scan.nextInt();
+		
+
+		System.out.printf("%d월은 %d일까지 있습니다.", inputMonth, maxDayOfMonth[inputMonth-1]);
 	}
 }
