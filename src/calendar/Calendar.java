918589e45e730d811e3b4aab4fd617ab71786fbb
MJbae <bmj5208@gmail.com>
2020-12-05 16:09:25 +0900

    mission7: 더 정교한 달력 찍기

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -1,14 +1,32 @@
 package calendar;
 
 import java.util.Scanner;
+import java.util.HashMap;
 
 public class Calendar {
 
+	
+	private static final String[] DATES = {"   ", "   ", "   ", "   ", "   ", "   ", "   ",
+			"  1", "  2", "  3", "  4", "  5", "  6", "  7", "  8", "  9", " 10", 
+			" 11", " 12", " 13", " 14", " 15", " 16", " 17", " 18", " 19", " 20", 
+			" 21", " 22", " 23", " 24", " 25", " 26", " 27", " 28", " 29", " 30"," 31"};
+	
+	HashMap<String, Integer> DAY_OF_WEEK_HASH = new HashMap<String, Integer>(){{
+		put("SU", 0);
+		put("MO", 1);
+		put("TU", 2);
+		put("WE", 3);
+		put("TH", 4);
+		put("FR", 5);
+		put("SA", 6);
+	}};
+	
 	private static final int[] MAX_DATE = { 31, 28, 30, 31, 30, 31, 31, 31, 30, 31, 30, 31 };
 	private static final int[] MAX_DATE_LEAN_YEAR = { 31, 29, 30, 31, 30, 31, 31, 31, 30, 31, 30, 31 };
-	private static final char[] DAY_OF_WEEK = { '일', '월', '화', '수', '목', '금', '토' };
+	private static final String[] DAY_OF_WEEK = {"SU", "MO", "TU", "WE", "TH", "FR", "SA" };
 	private static String PROMPT_YEAR = "YEAR> ";
 	private static String PROMPT_MONTH = "MONTH> ";
+	private static String PROMPT_WEEKDAY = "WEEKDAY> ";
 	
 	public boolean isLeapYear(int year) {
 		boolean answer = false;
@@ -40,21 +58,40 @@ public class Calendar {
 		}
 	}
 
-	public void showAllDays(int inputYear, int inputMonth) {
+	public void showAllDays(int inputYear, int inputMonth, String inputDay) {
 		int maxDay = getMaxDay(inputYear, inputMonth);
 
 		System.out.printf("    <<%d년 %d월>>    ", inputYear, inputMonth);
 		System.out.println(" ");
-		for (char eachDay : DAY_OF_WEEK) {
-			System.out.print(eachDay + "  ");
+		
+		int startPoint = DAY_OF_WEEK_HASH.get(inputDay);
+		
+		String[] copyOfDATES = new String[maxDay + startPoint + 1];
+
+		System.arraycopy(DATES, 7-startPoint, copyOfDATES, 0, copyOfDATES.length);
+		
+		for (String eachDay : DAY_OF_WEEK) {
+			System.out.print(" "+ eachDay);
 		}
 		System.out.println("\n---------------------");
-		for (int date = 1; date < maxDay + 1; date++) {
-			System.out.printf("%3d", date);
-			if (date % 7 == 0) {
+		
+		int indexForTap = 1;
+		for(String date : copyOfDATES) {
+			
+			System.out.print(date);
+			
+			if (indexForTap % 7 == 0) {
 				System.out.println(" ");
 			}
+			indexForTap++;
 		}
+		
+//		for (int date = 1; date <= maxDay; date++) {
+//			System.out.printf("%3d", date);
+//			if (date % 7 == 0) {
+//				System.out.println(" ");
+//			}
+//		}
 		System.out.println(" ");
 	}
 
@@ -64,6 +101,7 @@ public class Calendar {
 		Calendar cal = new Calendar();
 		int month = 0;
 		int year = 0;
+		String day = "";
 
 		while (true) {
 			System.out.println("연도를 입력하세요.");
@@ -75,6 +113,10 @@ public class Calendar {
 			System.out.println("월을 입력하세요.");
 			System.out.print(PROMPT_MONTH);
 			month = scan.nextInt();
+			
+			System.out.println("첫번째 요일을 입력하세요.");
+			System.out.print(PROMPT_WEEKDAY);
+			day = scan.next();
 
 			if (month == -1)
 				break;
@@ -82,7 +124,7 @@ public class Calendar {
 			if (month > 12 || month < 1)
 				continue;
 
-			cal.showAllDays(year, month);
+			cal.showAllDays(year, month, day);
 			System.out.println("");
 		}
 
